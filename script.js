5283a9b280db696decbabadadea162860d9bc2f7
Mark Valentino <mike9377mike9377@gmail.com>
2024-11-17 01:50:39 -0500

    improved bilinear interpolation

--- script.js
+++ script.js
@@ -49,6 +49,38 @@ function nearestNeighborScale(
   return imageToBeScaled;
 }
 
+// function applyBilinearScale(
+//   newWidth,
+//   newHeight,
+//   originalWidth,
+//   originalHeight,
+//   originalImgData
+// ) {
+//   scaledImg = fillWithTransparentPixels(newWidth, newHeight);
+
+//   // Interpolates in X direction
+//   for (let x = 0; x < originalWidth - 1; x++) {
+//     for (let y = 0; y < originalHeight; y++) {
+//       for (let channel = 0; channel < 4; channel++) {
+//         scaledImg[y * 2][x * 2][channel] = originalImgData[y][x][channel];
+//         scaledImg[y * 2][x * 2 + 1][channel] =
+//           (originalImgData[y][x][channel] + originalImgData[y][x + 1][channel]) / 2;
+//       }
+//     }
+//   }
+
+//   // Interpolates in Y direction
+//   for (let x = 0; x < newWidth; x++) {
+//     for (let y = 1; y < newHeight - 1; y+=2) {
+//       for (let channel = 0; channel < 4; channel++) {
+//         scaledImg[y][x][channel] = (scaledImg[y - 1][x][channel] + scaledImg[y + 1][x][channel]) / 2;
+//       }
+//     }
+//   }
+
+//   return scaledImg;
+// }
+
 function applyBilinearScale(
   newWidth,
   newHeight,
@@ -56,25 +88,64 @@ function applyBilinearScale(
   originalHeight,
   originalImgData
 ) {
-  scaledImg = fillWithTransparentPixels(newWidth, newHeight);
+  let scaledImg = fillWithTransparentPixels(newWidth, newHeight);
+  const xScaleFactor = originalWidth / newWidth;
+  const yScaleFactor = originalHeight / newHeight;
 
-  // Interpolates in X direction
-  for (let x = 0; x < originalWidth - 1; x++) {
-    for (let y = 0; y < originalHeight; y++) {
-      for (let channel = 0; channel < 4; channel++) {
-        scaledImg[y * 2][x * 2][channel] = originalImgData[y][x][channel];
-        scaledImg[y * 2][x * 2 + 1][channel] =
-          (originalImgData[y][x][channel] + originalImgData[y][x + 1][channel]) / 2;
-      }
-    }
-  }
+  for (let j = 0; j < newHeight; j++) {
+    for (let i = 0; i < newWidth; i++) {
+      // Map the coordinates back to the original image
+      const originalX = i * xScaleFactor;
+      const originalY = j * yScaleFactor;
 
-  // Interpolates in Y direction
-  for (let x = 0; x < newWidth; x++) {
-    for (let y = 1; y < newHeight - 1; y+=2) {
-      for (let channel = 0; channel < 4; channel++) {
-        scaledImg[y][x][channel] = (scaledImg[y - 1][x][channel] + scaledImg[y + 1][x][channel]) / 2;
-      }
+      const xFloor = Math.floor(originalX);
+      const xCeil = Math.min(originalWidth - 1, Math.ceil(originalX));
+      const yFloor = Math.floor(originalY);
+      const yCeil = Math.min(originalHeight - 1, Math.ceil(originalY));
+
+      // console.log("i:" + i  + "j:" + j + ` ${xFloor}, ${xCeil}, ${yFloor}, ${yCeil}`);
+
+      // Gets the fractional coordinates of the pixel to be interpolated
+      const xFrac = originalX - xFloor;
+      const yFrac = originalY - yFloor;
+
+      const weightX = 1 - xFrac;
+      const weightY = 1 - yFrac;
+
+      // Gets the four surrounding pixels
+      const topLeft = originalImgData[yFloor][xFloor];
+      const topRight = originalImgData[yFloor][xCeil];
+      const bottomLeft = originalImgData[yCeil][xFloor];
+      const bottomRight = originalImgData[yCeil][xCeil];
+
+      // Interpolate in the x-direction
+      const top = [
+        // R
+        topLeft[0] * weightX + topRight[0] * xFrac,
+        // G
+        topLeft[1] * weightX + topRight[1] * xFrac,
+        // B
+        topLeft[2] * weightX + topRight[2] * xFrac,
+        // A
+        topLeft[3] * weightX + topRight[3] * xFrac,
+      ];
+
+      const bottom = [
+        bottomLeft[0] * weightX + bottomRight[0] * xFrac,
+        bottomLeft[1] * weightX + bottomRight[1] * xFrac,
+        bottomLeft[2] * weightX + bottomRight[2] * xFrac,
+        bottomLeft[3] * weightX + bottomRight[3] * xFrac,
+      ];
+
+      // Interpolate in the y-direction
+      const pixel = [
+        top[0] * weightY + bottom[0] * yFrac,
+        top[1] * weightY + bottom[1] * yFrac,
+        top[2] * weightY + bottom[2] * yFrac,
+        top[3] * weightY + bottom[3] * yFrac,
+      ];
+
+      scaledImg[j][i] = pixel;
     }
   }
 
